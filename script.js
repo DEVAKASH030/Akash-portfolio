474922b6711d24f5520dd063fc32cb337beca760
DEVAKASH030 <dev.akash030@gmail.com>
2024-05-09 23:42:02 +0530

    update the text animation

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing_text", {
+    strings: ["Backend Developer", "Frontend Developer", "Wordpress Developer",],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+    cursorChar: '|',
+    });
+    
